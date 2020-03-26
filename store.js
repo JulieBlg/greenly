d9c61cd3b3a2e7c76b721e681c68eff8e81291b7
Julie Boulangée <julie.boulangee@gmail.com>
2020-03-26 17:22:18 +0100

    refactor(store): dry code and add switch to make it easier to read

--- store.js
+++ store.js
@@ -9,83 +9,70 @@ export class DiscountOffer {
 export class Store {
   constructor(discountOffers = []) {
     this.discountOffers = discountOffers;
+    this.partnersToIgnore = ["Ilek"];
+    this.specificPartners = ["Naturalia", "Vinted", "Ilek", "BackMarket"];
   }
 
   updateDiscounts() {
     for (var i = 0; i < this.discountOffers.length; i++) {
-      const isClassicPartner = ![
-        "Naturalia",
-        "Vinted",
-        "Ilek",
-        "BackMarket"
-      ].includes(this.discountOffers[i].partnerName);
+      const isNotPartnerToIgnore = !this.partnersToIgnore.includes(
+        this.discountOffers[i].partnerName
+      );
 
-      if (this.discountOffers[i].partnerName !== "Ilek") {
+      if (isNotPartnerToIgnore) {
+        //UPDATE DAYS BEFORE EXPIRATION
         this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
 
-      if (isClassicPartner) {
-        this.discountOffers[i] = this.generateDiscountForClassicPartners(
-          this.discountOffers[i]
-        );
-      } else {
-        if (this.discountOffers[i].partnerName === "BackMarket")
-          this.discountOffers[i] = this.generateDiscountForBackMarket(
-            this.discountOffers[i]
-          );
+        //CALCULATE CURRENT DISCOUNT PERCENTAGE
+        let percentage = this.calculateDiscountOfTheDay(this.discountOffers[i]);
 
-        if (this.discountOffers[i].partnerName === "Vinted")
-          this.discountOffers[i] = this.generateDiscountForVinted(
-            this.discountOffers[i]
-          );
+        //CHECK CONDITIONS ON PERCENTAGE
+        if (percentage > 50) percentage = 50;
+        else if (percentage < 0) percentage = 0;
 
-        if (this.discountOffers[i].partnerName === "Naturalia")
-          this.discountOffers[i] = this.generateDiscountForNaturalia(
-            this.discountOffers[i]
-          );
+        this.discountOffers[i].discountInPercent = percentage;
       }
     }
 
     return this.discountOffers;
   }
 
-  generateDiscountForClassicPartners(discountOffer) {
-    var { partnerName, expiresIn, discountInPercent } = discountOffer;
+  calculateDiscountOfTheDay(discountOffer) {
+    const isExpired = discountOffer.expiresIn < 0;
+    let percentage = discountOffer.discountInPercent;
+    const percentageOfTheDay = isExpired ? 2 : 1;
 
-    if (discountInPercent > 0) {
-      discountInPercent -= 1;
+    if (!this.specificPartners.includes(discountOffer.partnerName)) {
+      //CLASSIC PARTNERS
+      percentage -= percentageOfTheDay;
+    } else {
+      //SPECIFIC PARTNERS
+      switch (discountOffer.partnerName) {
+        case "BackMarket":
+          percentage -= percentageOfTheDay * 2;
+          break;
+        case "Vinted":
+          percentage = this.generatePercentageForVinted(
+            percentage,
+            discountOffer.expiresIn
+          );
+          break;
+        case "Naturalia":
+          percentage += percentageOfTheDay;
+          break;
+      }
     }
 
-    if (expiresIn < 0 && discountInPercent > 0) {
-      discountInPercent -= 1;
-    }
-
-    return { partnerName, expiresIn, discountInPercent };
+    return percentage;
   }
 
-  generateDiscountForBackMarket(discountOffer) {
-    var { partnerName, expiresIn, discountInPercent } = discountOffer;
-
-    if (discountInPercent > 1) {
-      discountInPercent -= 2;
-    }
-
-    if (expiresIn < 0 && discountInPercent > 1) {
-      discountInPercent -= 2;
-    }
-
-    return { partnerName, expiresIn, discountInPercent };
-  }
-
-  generateDiscountForVinted(discountOffer) {
-    var { partnerName, expiresIn, discountInPercent } = discountOffer;
-
+  generatePercentageForVinted(discountInPercent, expiresIn) {
     if (expiresIn < 0) {
       discountInPercent = 0;
     } else {
-      if (expiresIn < 11 && discountInPercent < 49) {
+      if (expiresIn < 11) {
         discountInPercent += 2;
-        if (expiresIn < 6 && discountInPercent < 50) {
+        if (expiresIn < 6) {
           discountInPercent += 1;
         }
       } else {
@@ -93,22 +80,6 @@ export class Store {
       }
     }
 
-    return { partnerName, expiresIn, discountInPercent };
-  }
-
-  generateDiscountForNaturalia(discountOffer) {
-    var { partnerName, expiresIn, discountInPercent } = discountOffer;
-
-    if (discountInPercent < 50) {
-      discountInPercent += 1;
-    }
-
-    if (expiresIn < 0) {
-      if (discountInPercent < 50) {
-        discountInPercent += 1;
-      }
-    }
-
-    return { partnerName, expiresIn, discountInPercent };
+    return discountInPercent;
   }
 }
