af57ec9d552571f31de66a22ad3e8e95fca1d085
Julie Boulangée <julie.boulangee@gmail.com>
2020-03-26 15:51:54 +0100

    refactor(store): dry code

--- store.js
+++ store.js
@@ -10,6 +10,7 @@ export class Store {
   constructor(discountOffers = []) {
     this.discountOffers = discountOffers;
   }
+
   updateDiscounts() {
     for (var i = 0; i < this.discountOffers.length; i++) {
       const isClassicPartner = ![
@@ -19,61 +20,29 @@ export class Store {
         "BackMarket"
       ].includes(this.discountOffers[i].partnerName);
 
+      if (this.discountOffers[i].partnerName !== "Ilek") {
+        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
+      }
+
       if (isClassicPartner) {
         this.discountOffers[i] = this.generateDiscountForClassicPartners(
           this.discountOffers[i]
         );
       } else {
-        var { partnerName, expiresIn, discountInPercent } = this.discountOffers[
-          i
-        ];
+        if (this.discountOffers[i].partnerName === "BackMarket")
+          this.discountOffers[i] = this.generateDiscountForBackMarket(
+            this.discountOffers[i]
+          );
 
-        if (partnerName === "Naturalia") {
-          if (discountInPercent < 50) {
-            discountInPercent = discountInPercent + 1;
-          }
-        }
+        if (this.discountOffers[i].partnerName === "Vinted")
+          this.discountOffers[i] = this.generateDiscountForVinted(
+            this.discountOffers[i]
+          );
 
-        if (partnerName === "BackMarket") {
-          if (discountInPercent > 1) {
-            discountInPercent = discountInPercent - 2;
-          }
-        }
-
-        if (partnerName === "Vinted") {
-          if (expiresIn < 11) {
-            if (discountInPercent < 49) {
-              discountInPercent = discountInPercent + 2;
-            }
-          }
-          if (expiresIn < 6) {
-            if (discountInPercent < 50) {
-              discountInPercent = discountInPercent + 1;
-            }
-          }
-        }
-
-        if (partnerName !== "Ilek") expiresIn = expiresIn - 1;
-
-        if (expiresIn < 0) {
-          if (partnerName === "Vinted") {
-            discountInPercent = 0;
-          }
-
-          if (partnerName === "BackMarket") {
-            if (discountInPercent > 1) {
-              discountInPercent = discountInPercent - 2;
-            }
-          }
-
-          if (partnerName === "Naturalia") {
-            if (discountInPercent < 50) {
-              discountInPercent = discountInPercent + 1;
-            }
-          }
-        }
-
-        this.discountOffers[i] = { partnerName, expiresIn, discountInPercent };
+        if (this.discountOffers[i].partnerName === "Naturalia")
+          this.discountOffers[i] = this.generateDiscountForNaturalia(
+            this.discountOffers[i]
+          );
       }
     }
 
@@ -84,14 +53,57 @@ export class Store {
     var { partnerName, expiresIn, discountInPercent } = discountOffer;
 
     if (discountInPercent > 0) {
-      discountInPercent = discountInPercent - 1;
+      discountInPercent -= 1;
     }
 
-    expiresIn = expiresIn - 1;
+    if (expiresIn < 0 && discountInPercent > 0) {
+      discountInPercent -= 1;
+    }
+
+    return { partnerName, expiresIn, discountInPercent };
+  }
+
+  generateDiscountForBackMarket(discountOffer) {
+    var { partnerName, expiresIn, discountInPercent } = discountOffer;
+
+    if (discountInPercent > 1) {
+      discountInPercent -= 2;
+    }
+
+    if (expiresIn < 0 && discountInPercent > 1) {
+      discountInPercent -= 2;
+    }
+
+    return { partnerName, expiresIn, discountInPercent };
+  }
+
+  generateDiscountForVinted(discountOffer) {
+    var { partnerName, expiresIn, discountInPercent } = discountOffer;
 
     if (expiresIn < 0) {
-      if (discountInPercent > 0) {
-        discountInPercent = discountInPercent - 1;
+      discountInPercent = 0;
+    } else {
+      if (expiresIn < 11 && discountInPercent < 49) {
+        discountInPercent += 2;
+      }
+      if (expiresIn < 6 && discountInPercent < 50) {
+        discountInPercent += 1;
+      }
+    }
+
+    return { partnerName, expiresIn, discountInPercent };
+  }
+
+  generateDiscountForNaturalia(discountOffer) {
+    var { partnerName, expiresIn, discountInPercent } = discountOffer;
+
+    if (discountInPercent < 50) {
+      discountInPercent += 1;
+    }
+
+    if (expiresIn < 0) {
+      if (discountInPercent < 50) {
+        discountInPercent += 1;
       }
     }
 
